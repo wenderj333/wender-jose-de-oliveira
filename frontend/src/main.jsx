f8fc699182892391c02b536f539735f2dbe454dc
Wenderj333 <wenderj333@gmail.com>
2026-02-17 12:25:40 +0100

    fix: protecao contra conflito Google Translate + React DOM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,25 @@ import { WebSocketProvider } from './context/WebSocketContext';
 import './i18n';
 import './styles/main.css';
 
+// Protect React from Google Translate / Chrome auto-translate DOM mutations
+// that cause "Failed to execute 'insertBefore' / 'removeChild' on 'Node'" errors
+if (typeof Node !== 'undefined') {
+  const origInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, refNode) {
+    if (refNode && refNode.parentNode !== this) {
+      return newNode;
+    }
+    return origInsertBefore.call(this, newNode, refNode);
+  };
+  const origRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      return child;
+    }
+    return origRemoveChild.call(this, child);
+  };
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
