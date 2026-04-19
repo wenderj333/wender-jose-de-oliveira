7290d337cdb719fd94f7f2b79cbf995e57c5ec01
Wender <wenderj333@gmail.com>
2026-04-19 15:39:44 +0200

    Code splitting - reduzir chunk size para mobile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,29 @@
-﻿import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from ' + chr(39) + 'vite' + chr(39) + ';
+import react from ' + chr(39) + '@vitejs/plugin-react' + chr(39) + ';
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [' + chr(39) + 'react' + chr(39) + ', ' + chr(39) + 'react-dom' + chr(39) + ', ' + chr(39) + 'react-router-dom' + chr(39) + '],
+          firebase: [' + chr(39) + 'firebase/app' + chr(39) + ', ' + chr(39) + 'firebase/auth' + chr(39) + '],
+          i18n: [' + chr(39) + 'react-i18next' + chr(39) + ', ' + chr(39) + 'i18next' + chr(39) + '],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600,
+  },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'https://sigo-com-fe-api.onrender.com',
+      ' + chr(39) + '/api' + chr(39) + ': {
+        target: ' + chr(39) + 'https://sigo-com-fe-api.onrender.com' + chr(39) + ',
         changeOrigin: true,
         secure: false,
       },
-      '/ws': {
-        target: 'wss://sigo-com-fe-api.onrender.com',
+      ' + chr(39) + '/ws' + chr(39) + ': {
+        target: ' + chr(39) + 'wss://sigo-com-fe-api.onrender.com' + chr(39) + ',
         ws: true,
         changeOrigin: true,
         secure: false,
