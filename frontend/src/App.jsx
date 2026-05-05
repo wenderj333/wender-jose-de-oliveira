74e0ea0f41f08da1f40f939be86a8ea26cff58bb
Wender <wenderj333@gmail.com>
2026-05-05 10:28:36 +0200

    Fix - import React em falta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 ﻿import { useState, useEffect, useRef } from "react";
 import "./sidebar.css";
 import { Routes, Route, Link, useNavigate, useLocation } from "react-router-dom";
