760663cd08571bd5167b10c5c78149864a0943f2
Wender <wenderj333@gmail.com>
2026-05-17 15:51:03 +0200

    Add: notificações push Firebase FCM

---
+++ backend/src/services/notifications.js
@@ -0,0 +1,21 @@
+﻿const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-adminsdk.json');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+
+async function sendPushNotification(fcmToken, title, body, data = {}) {
+  try {
+    const message = { notification: { title, body }, data, token: fcmToken };
+    const response = await admin.messaging().send(message);
+    console.log('Notificacao enviada:', response);
+    return response;
+  } catch (error) {
+    console.error('Erro ao enviar notificacao:', error);
+  }
+}
+
+module.exports = { sendPushNotification };
